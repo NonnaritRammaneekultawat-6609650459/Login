aaaca5a51b8390b5861905606f1c49858fb4918e
Nonnarit Rammaneekultawat <nonnarit.ram@dome.tu.ac.th>
2024-10-18 17:21:58 +0700

    first commit

---
+++ Frontend/public/js/script.js
@@ -0,0 +1,37 @@
+function submitLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Application-Key' : 'TU02f234f19d6c01ed419d45722e8ebf4c631e5ea67a955e849828e38ce710359f9c2afd624f216c2ba1597f46b08ca8c5'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('message').innerText = data.message;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+
+function call_REST_API_Hello() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const url = (
+        'http://localhost:8080/hello?' +
+        new URLSearchParams({ myName: username, lastName: password}).toString()
+      );
+    
+    fetch(url)
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('message').innerText = text;
+    })
+    .catch(error => console.error('Error:', error));
+}
